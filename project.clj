a6e85f60fce39cad37ead47bb591ac2d6bf54402
Chee Sing Lee <lee@bigml.com>
2016-08-10 09:48:12 -0700

    add to bigml group

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject test-helpers "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject bigml/test-helpers "0.1.0"
+  :description "A Leiningen plugin to help with running unit tests"
+  :url "https://github.com/bigmlcom/test-helpers"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :eval-in-leiningen true)
