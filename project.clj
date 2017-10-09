7eb0c315ce57663a99f331fd0861bd3144ce0c9c
jao <jao@bigml.com>
2017-10-09 23:17:06 +0200

    Adding new s3-based libs repository and version bump

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject bigml/test-helpers "0.1.0"
+(defproject bigml/test-helpers "0.1.1"
   :description "A Leiningen plugin to help with running unit tests"
   :url "https://github.com/bigmlcom/test-helpers"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :deploy-repositories {"releases" {:url "s3://bigml-development-wmlibs/repo/"
+                                    :username :env/aws_access_key_id
+                                    :passphrase :env/aws_secret_access_key
+                                    :sign-releases false}}
+  :plugins [[s3-wagon-private "1.3.0"]]
   :eval-in-leiningen true)
