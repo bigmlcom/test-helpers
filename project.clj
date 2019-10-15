e7132d3f0e650d23ebee13d25626e2cc661662da
Charles Parker <parker@bigml.com>
2019-10-15 12:25:22 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :deploy-repositories {"releases" {:url "s3://bigml-development-wmlibs/repo/"
+                                    :private-key-file ""
                                     :username :env/aws_access_key_id
                                     :passphrase :env/aws_secret_access_key
                                     :sign-releases false}}
-  :plugins [[s3-wagon-private "1.3.0"]]
+  :plugins [[s3-wagon-private "1.3.2"]]
   :eval-in-leiningen true)
